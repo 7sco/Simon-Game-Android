988aa7e438cef5413340e0a3e5d91baf5525d764
Francisco Andrade <xaviandrade14@gmail.com>
2017-10-30 21:37:58 -0400

    Colors randomise and timers working

--- app/src/main/java/nyc/c4q/simongame/MainActivity.java
+++ app/src/main/java/nyc/c4q/simongame/MainActivity.java
@@ -1,7 +1,10 @@
 package nyc.c4q.simongame;
 
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -11,7 +14,14 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -19,7 +29,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView score, level, sequenceText;
     private LinearLayout appBkg;
     boolean button1, button2, button3,button4;
-    boolean start;
+    boolean start, wait;
 
     private  ArrayList<Integer>  lista = new ArrayList<>();
 
@@ -31,6 +41,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     int counter;
+
+    int count;
+
+    int numbers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,12 +81,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     //all buttons sequence right or wrong
     public void isFull(){
 
-        if (counter >lista.size()-1 ){
+        if ( lista.size() ==counter){
 
             for(int i=0; i<lista.size();i++){
 
 
-                if(lista.get(i)==lista2.get(i)){
+                if(lista.get(i)== lista2.get(i)){
                     score.setText("+100");
                     break;
                 }
@@ -93,6 +107,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+public void timer(int num){
+
+    btn1.setText("");
+    btn1.setBackgroundColor(getResources().getColor(R.color.color1Off));
+    btn2.setText("");
+    btn2.setBackgroundColor(getResources().getColor(R.color.color2Off));
+    btn3.setText("");
+    btn3.setBackgroundColor(getResources().getColor(R.color.color3Off));
+    btn4.setText("");
+    btn4.setBackgroundColor(getResources().getColor(R.color.color4Off));
+
+
+        switch (num){
+            case 1:
+                btn1.setText("1");
+                btn1.setBackgroundColor(getResources().getColor(R.color.color1On));
+                break;
+            case 2:
+                btn2.setText("2");
+                btn2.setBackgroundColor(getResources().getColor(R.color.color2On));
+                break;
+            case 3:
+                btn3.setText("3");
+                btn3.setBackgroundColor(getResources().getColor(R.color.color3On));
+                break;
+            case 4:
+                btn4.setText("4");
+                btn4.setBackgroundColor(getResources().getColor(R.color.color4On));
+                break;
+
+        }
+
+}
+
+
     @Override
     public void onClick(View view) {
 
@@ -106,6 +155,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             //resets or starts the game
             case R.id.reset:
 
+
                 sequenceText.setText("");
                 txt="";
 
@@ -117,21 +167,55 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 //Shuffles numbers inside main list lista
                 Collections.shuffle(lista);
 
-                //gives an order text to each button
-                btn1.setText("1");
-                btn2.setText("2");
-                btn3.setText("3");
-                btn4.setText("4");
-
-
                 // gets lista size and throws back the sequence, I am printing this at the bottom for reference
+
+                final Queue <Integer> listarandom = new LinkedList<>();
 
                 for(int i=0;i<lista.size();i++){
 
+                    listarandom.offer(lista.get(i));
                     txt = txt + lista.get(i)+"-";
 
                     sequenceText.setText(txt);
                 }
+
+
+
+
+                final Handler handler = new Handler();
+                count = 0;
+
+                final Runnable runnable = new Runnable() {
+                    public void run() {
+
+                        // need to do tasks on the UI thread
+
+
+                        if (count < 4) {
+                            timer(listarandom.poll());
+                            handler.postDelayed(this, 1500);
+                            wait=true;
+                        }
+
+
+                        else{
+                            btn1.setText("");
+                            btn1.setBackgroundColor(getResources().getColor(R.color.color1Off));
+                            btn2.setText("");
+                            btn2.setBackgroundColor(getResources().getColor(R.color.color2Off));
+                            btn3.setText("");
+                            btn3.setBackgroundColor(getResources().getColor(R.color.color3Off));
+                            btn4.setText("");
+                            btn4.setBackgroundColor(getResources().getColor(R.color.color4Off));
+                            wait=false;
+
+                        }
+
+                        count++;
+                    }
+                };
+                // trigger first time
+                handler.post(runnable);
 
                 //This allows other buttons to be responsive, if user did not start the game all 4 buttons will not do anyhting
                 start=true;
@@ -140,13 +224,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 //All buttons will add to the lista2 its valus to be checked with the isFull() method at the end when 4 presses have been made
             case R.id.btn1:
 
+                if(wait){
+                    Toast toast = Toast.makeText(getApplicationContext(), "Wait", Toast.LENGTH_SHORT);
+                    toast.show();
+                    break;
+                }
+                if(start && lista2.size()<=lista.size()){
 
-                if(start){
                     currentNum=1;
                     lista2.add(currentNum);
                     counter ++;
                     isFull();
                 }
+
 
                 //if game has not been started we are showing a toast asking the use to start the game to play
                 else{
@@ -158,8 +248,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             case R.id.btn2:
+                if(wait){
+                    Toast toast = Toast.makeText(getApplicationContext(), "Wait", Toast.LENGTH_SHORT);
+                    toast.show();
+                    break;
+                }
 
-                if(start){
+                if(start&& lista2.size()<=lista.size()){
                     currentNum=2;
 
                     lista2.add(currentNum);
@@ -175,13 +270,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             case R.id.btn3:
-                if(start){
+                if(wait){
+                    Toast toast = Toast.makeText(getApplicationContext(), "Wait", Toast.LENGTH_SHORT);
+                    toast.show();
+                    break;
+                }
+                if(start&& lista2.size()<=lista.size()){
                     currentNum=3;
 
                     lista2.add(currentNum);
                     counter ++;
                     isFull();
                 }
+
 
 
                 else {
@@ -193,14 +294,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             case R.id.btn4:
-
-                if(start){
+                if(wait){
+                    Toast toast = Toast.makeText(getApplicationContext(), "Wait", Toast.LENGTH_SHORT);
+                    toast.show();
+                    break;
+                }
+                if(start&& lista2.size()<=lista.size()){
                     currentNum=4;
 
                     lista2.add(currentNum);
                     counter ++;
                     isFull();
                 }
+
 
                 else{
                     Toast toast = Toast.makeText(getApplicationContext(), "Start App first", Toast.LENGTH_SHORT);
